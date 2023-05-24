ae85ea2cccf33fd0aa081bc212ced1a809bb7cf7
qlwong <730402725@qq.com>
2023-05-24 22:02:55 +0800

    modified:   .eslintrc.cjs 	modified:   src/App.vue 	new file:   src/apis/testApi.js 	new file:   src/assets/images/200.png 	new file:   src/assets/images/center-bg.png 	new file:   src/assets/images/load.gif 	new file:   src/assets/images/loading.gif 	new file:   src/assets/images/login-bg.png 	new file:   src/assets/images/logo.png 	new file:   src/assets/images/none.png 	new file:   src/assets/images/qrcode.jpg 	modified:   src/main.js 	modified:   src/router/index.js 	new file:   src/styles/common.scss 	new file:   src/utils/http.js 	deleted:    src/views/AboutView.vue 	deleted:    src/views/HomeView.vue 	new file:   src/views/category/index.vue 	new file:   src/views/home/index.vue 	new file:   src/views/layout/index.vue 	new file:   src/views/login/index.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
+
 </script>
 
 <template>
-  <el-button type="primary">Primary</el-button>
+  <RouterView/>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { getCategory } from './apis/testApi'
+import '@/styles/common.scss'
+
+getCategory().then(res=>{
+    console.log(res)
+})
 
 const app = createApp(App)
 
